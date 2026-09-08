f6635f4982146c201311d8b4d1ac14b9b8370ab2
Alex Yudi <yuditoma@hotmail.com>
2026-09-08 20:28:01 -0300

    fix: corrige versão do Java para 25 no pom.xml e modifica estrutura do projeto

--- src/main/java/com/br/back/bairro/api/controllers/ExampleController.java
+++ src/main/java/com/br/back/bairro/api/controllers/ExampleController.java
@@ -1,4 +1,4 @@
-package com.br.back.bairro.controllers;
+package com.br.back.bairro.api.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
