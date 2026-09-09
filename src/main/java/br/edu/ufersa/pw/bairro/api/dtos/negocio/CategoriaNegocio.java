a9b3a59f0f4406d9645ef184213737cac3317163
Alex Yudi <yuditoma@hotmail.com>
2026-09-09 19:57:30 -0300

    feat: cria enum para categorias dos negócios cadastrados

---
+++ src/main/java/br/edu/ufersa/pw/bairro/api/dtos/negocio/CategoriaNegocio.java
@@ -0,0 +1,11 @@
+package br.edu.ufersa.pw.bairro.api.dtos.negocio;
+
+
+// TODO: Rever esse enum quando a API estiver mais avançada. A ideia é poder ter essa parte dinâmica, com o usuário podendo dar sugestões de novas categorias, conforme seu negócio
+public enum CategoriaNegocio {
+    HORTIFRUTI,
+    MERCADO,
+    SERVICOS,
+    PADARIA,
+    OUTRO
+}
